4618cd2cb371eac6cacf6c6a04fc43902b40a9c5
Daniyar <danik14039@gmail.com>
2023-03-07 00:29:23 +0600

    Added Example003 for divide command

---
+++ Example003_div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(0,10);
+Console.WriteLine(numberA);
+double numberB=new Random().Next(0,10);
+Console.WriteLine(numberB);
+double div=numberA/numberB;
+Console.WriteLine(div);
