2623d5c843bcb8bc1037feac8d64303a464ea31b
Daniyar <danik14039@gmail.com>
2023-03-08 00:19:16 +0600

    Example009 While

---
+++ Example009/Program.cs
@@ -0,0 +1,12 @@
+﻿// . Напишите программу, которая на вход принимает одно число 
+// (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberNeg = -1*number;
+while(numberNeg<=number)
+{
+    Console.WriteLine(numberNeg);
+    numberNeg=numberNeg+1;
+}
