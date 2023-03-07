3f4b3198e6afa217efa336829576f533e7e8c89e
Daniyar <danik14039@gmail.com>
2023-03-08 00:08:49 +0600

    Example007 If operator

---
+++ Example007/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1==number2*number2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
