7f1472c9c8063d94f1c224fe9f3dbf4b9080e2f6
Daniyar <danik14039@gmail.com>
2023-03-08 00:09:36 +0600

    Example008 If-Else_If-Else

---
+++ Example008/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру
+
+Console.WriteLine("Введите число от 1 до 7");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if(number1==1)
+{
+    Console.WriteLine("Monday");
+}
+else if(number1==2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if(number1==3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if(number1==4)
+{
+    Console.WriteLine("Thursday");
+}
+else if(number1==5)
+{
+    Console.WriteLine("Friday");
+}
+else if(number1==6)
+{
+    Console.WriteLine("Saturday");
+}
+else if(number1==7)
+{
+    Console.WriteLine("Sunday");
+}
+
+else
+{
+    Console.WriteLine("Такого дня недели у нас нет");
+}
