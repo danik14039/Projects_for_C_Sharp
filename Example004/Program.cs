cb3bb6be9f4727387024606a053f31ac14a7cc21
Daniyar <danik14039@gmail.com>
2023-03-07 19:53:50 +0600

    added example004 Dog issue

---
+++ Example004/Program.cs
@@ -0,0 +1,24 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+int t = 0;
+while (distance>1000)
+{
+    if (friend==1)
+    {
+        t = distance/(firstFriendSpeed+dogSpeed);
+        friend=2;
+        
+    }
+    else
+    {
+        t = distance/(secondFriendSpeed+dogSpeed);
+        friend=1;
+    }
+    distance=distance-(firstFriendSpeed+secondFriendSpeed)*t;
+    count=count+1;
+}
+Console.WriteLine(count);
