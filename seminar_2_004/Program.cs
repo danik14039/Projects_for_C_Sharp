99586a6b2d5b69166ba6d37732c1407903db824f
Daniyar <danik14039@gmail.com>
2023-03-11 00:46:18 +0600

    Added seminar2_004

---
+++ seminar_2_004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1==num2*num2)
+{
+    Console.WriteLine(num1 + " является квадратом " + num2);
+}
+else if  (num2==num1*num1)
+{
+    Console.WriteLine(num2 + " является квадратом " + num1);
+}
+else
+{
+    Console.WriteLine("Не являются квадратом");
+}
