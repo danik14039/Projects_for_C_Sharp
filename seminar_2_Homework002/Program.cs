13c3293237002ba16877dbd3f2466248137fe7a3
Daniyar <danik14039@gmail.com>
2023-03-11 19:59:42 +0600

    Added Homework002 of seminar2

---
+++ seminar_2_Homework002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+int num = new Random().Next(10,200);
+Console.WriteLine(num);
+string str = Convert.ToString(num);
+
+if (str.Length<3)
+{
+    Console.WriteLine("Третьего цифры нет");
+}
+else
+{
+    char thirdChar = str[2];
+    Console.WriteLine(thirdChar);
+}
