f4e305797460e2b5ed3ad5f16731ef6d59dc799a
Daniyar <danik14039@gmail.com>
2023-03-15 20:47:50 +0600

    Added seminar_3_Homework

---
+++ seminar_3_Homework001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+while (true)
+{
+    Console.WriteLine("Введите пятизначное число");
+    string num = Console.ReadLine();
+    
+    if(num.Length!=5)
+    {
+        Console.WriteLine("Это не пятизначное число");
+    }
+    if(num.Length==5)
+    {
+        char first = num[0];
+        char second = num[1];
+        char fourth = num[3];
+        char fifth = num[4];
+        if((first==fifth)&&(second==fourth))
+        {
+            Console.WriteLine("Это число является палидромом");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Это число не является палидромом");
+            break;
+        }
+    }   
+}
---
+++ seminar_3_Homework002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter coordinates of the first point X1, Y1, Z1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinates of the second point X2, Y2, Z2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round((Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2))),2);
+Console.WriteLine(distance);
