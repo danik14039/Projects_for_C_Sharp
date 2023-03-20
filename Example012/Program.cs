b24e368c9d1358320f6cb082ea210a798fa3b95f
Daniyar <danik14039@gmail.com>
2023-03-20 19:12:44 +0600

    Examples 012,013,014

---
+++ Example012/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int num1, int num2)
+{
+    int power = 1;
+    for(int i = 0; i<num2; i++)
+    {
+        power = power*num1;
+    }
+    return power;
+}
+
+bool ValidateNum2(int num2)
+{
+    if (num2<0)
+    {
+        Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int number1 = Prompt("Enter number 1: ");
+int number2 = Prompt("Enter number 2: ");
+if(ValidateNum2(number2))
+{
+    Console.WriteLine($"Число {number1} в степени {number2} равно {Power(number1,number2)}");
+}
---
+++ Example013_Methods/Program.cs
@@ -0,0 +1,55 @@
+﻿// Виды методов
+// Вид№1 ничего не принимает, ничего не возвращает
+
+void Method1()
+{
+    Console.WriteLine("My text...");
+}
+// Method1();
+
+// Вид №2 Что-то принимает, ничего не возвращает
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(msg:"Text message...");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+        
+    }
+}
+// Method21(msg:"Text message 2 ...", count: 2);
+
+// Вид №3 Ничего не принимают, что-то возвращают
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид №4 Что-то принимают, что-то возвращают
+
+string Method4(int count, string text)
+{
+    int i=0;
+    string result = String.Empty;
+
+    while(i<count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
