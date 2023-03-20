0cfc10f1b479f4793f2996aed88ecfc15df9e573
Daniyar <danik14039@gmail.com>
2023-03-20 13:39:28 +0600

    Seminar_4 added

---
+++ seminar_4_001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая принимает
+// на вход число (А) и выдает сумму числел от 1 до А
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNum()
+{
+    int sum = 0;
+    int i=0;
+    
+    while(i<num)
+    {
+        i++;
+        sum = sum + i;
+    }
+    return sum;
+}
+
+// int number = GetSumNum();
+// Console.WriteLine(number);
+Console.WriteLine(GetSumNum());
---
+++ seminar_4_002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, принимает на вход число и показывает количество цифр в числе
+// First variant
+// Console.WriteLine("Enter number: ");
+// string num = Console.ReadLine();
+// Console.WriteLine(num.Length);
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(num>0)
+{
+    num/=10;
+    count++;
+}
+Console.WriteLine($"Number length equal to = {count}");
