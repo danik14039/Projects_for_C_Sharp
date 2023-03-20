09b751feb18ba677cb44d5a5184bffc265e0ec40
Daniyar <danik14039@gmail.com>
2023-03-20 13:40:47 +0600

    Example 011 added

---
+++ Example011/Program.cs
@@ -0,0 +1,26 @@
+﻿// Console.WriteLine("Enter number: ");
+// string num = Console.ReadLine();
+// char [] array = new char [num.Length];
+// int sum = 0;
+
+// for(int i=0; i<num.Length; i++)
+// {
+//     array[i] = num[i];
+// }
+
+// for (int count=0; count<array.Length; count++)
+// {
+//     sum = sum + array[count];
+// }
+
+// Console.Write(sum);
+
+Console.WriteLine("Enter number: ");
+string num = Console.ReadLine();
+char [] array = num.ToCharArray();
+int sum = 0;
+for (int i=0; i<array.Length; i++)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine(sum);
